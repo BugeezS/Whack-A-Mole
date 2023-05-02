7ed1b97f0e9fc6ca7f0c138a16889e230a4a7e77
Bruno <brunolb.lafont@gmail.com>
2023-05-02 15:59:21 +0200

    add time select

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 import { divcreate } from "./divCreate.js";
 import { randomMole } from "./moleSelector.js";
+const intervalSelect = document.getElementById("interval-select");
+
+intervalSelect.addEventListener("change", () => {
+  const selectedValue = intervalSelect.value;
+  clearInterval(intervalId);
+  intervalId = setInterval(randomMole, selectedValue);
+});
+
+let intervalId = setInterval(randomMole, 500);
 
 for (let i = 0; i < 12; i++) {
   divcreate();
 }
-setInterval(randomMole, 500);
