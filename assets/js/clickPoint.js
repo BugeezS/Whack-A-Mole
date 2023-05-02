88d7537c1b53a52b581c1b1bd320dd40f2b85e6c
Bruno <brunolb.lafont@gmail.com>
2023-05-02 12:29:31 +0200

    add click

---
+++ assets/js/clickPoint.js
@@ -0,0 +1,5 @@
+export function clickPoint() {
+  const point = 0;
+  point + 1;
+  console.log(point);
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 import { divcreate } from "./divCreate.js";
 import { randomMole } from "./moleSelector.js";
+import { clickPoint } from "./clickPoint.js";
 
-for (let i = 0; i < 12; i++) {
-  divcreate();
-}
-setInterval(randomMole, 3000);
+document.addEventListener("DOMContentLoaded", function () {
+  for (let i = 0; i < 12; i++) {
+    divcreate();
+  }
+  setInterval(randomMole, 500);
+
+  const taupe = document.querySelector(".taupe");
+  taupe.addEventListener("click", clickPoint);
+
+  console.log(taupe);
+});
